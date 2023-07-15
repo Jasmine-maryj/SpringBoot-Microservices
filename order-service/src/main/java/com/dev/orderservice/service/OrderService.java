96a53e1ebcfa456b74b79095520c8239b4006bd6
Jasmine Mary <jasminemary630@gmail.com>
2023-07-15 22:28:06 +0530

    Discovery Server Added

--- order-service/src/main/java/com/dev/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/dev/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.dev.orderservice.service;
 
+import com.dev.orderservice.dto.InventoryResponse;
 import com.dev.orderservice.dto.OrderLineItemsDTO;
 import com.dev.orderservice.dto.OrderRequest;
 import com.dev.orderservice.entity.Order;
@@ -8,7 +9,9 @@ import com.dev.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +21,8 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -29,7 +34,22 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodeList =  order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
+
+        InventoryResponse[] available = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodeList).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean isProductsInStock = Arrays.stream(available).allMatch(inventoryResponse -> inventoryResponse.isInStock());
+
+        if(isProductsInStock) {
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock");
+        }
     }
 
     private OrderLineItems mapToDTO(OrderLineItemsDTO orderLineItemsDTO) {
--- product-service/src/main/java/com/dev/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/dev/productservice/ProductServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = ProductRepository.class)
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
